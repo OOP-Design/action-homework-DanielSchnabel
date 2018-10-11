89c79dfd615d16797fb2ab8d1f2d772f8fe99248
Eric Ebert <eberte@casady.org>
2018-10-11 11:16:33 -0500

    Comments and Suggestions

--- Button.java
+++ Button.java
@@ -9,17 +9,22 @@ import javax.imageio.*;
 public class Button{
     public static void main(String[] args){
         ButtonFrame frame = new ButtonFrame();
-        ButtonPanel panel = new ButtonPanel();
-        frame.add(panel);
         frame.setVisible(true);
     }
 }
 
 class ButtonFrame extends JFrame{
+    ButtonPanel panel = new ButtonPanel();
+    /**
+    Give the key listener to the frame. This was the tactic used in the typing game.
+    You can then call a panel method to add the key to the array list since the panel is
+    now an attribute of the frame.
+    **/
     public ButtonFrame(){
         super("Button");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(700,700);
+        frame.add(panel);
     }
 }
 
@@ -31,13 +36,8 @@ class ButtonPanel extends JPanel{
     public int y;
 
     public ButtonPanel(){
-        addKeyListener(new KeyAdapter(){
-            public void keyPressed(KeyEvent e){
-                String pressedKey = e.getKeyText(e.getKeyCode());
-                charArray.add(pressedKey);
-                repaint();
-            }
-        });
+	#Removed Key Listener... use the one in the frame
+
         addMouseListener(new MouseAdapter(){
             public void mouseClicked(MouseEvent e){
                 if(e.getButton() == MouseEvent.BUTTON3){
@@ -46,6 +46,8 @@ class ButtonPanel extends JPanel{
             }
         });
     }
+    
+    # You can write a method to add a pressed key in the array list.
 
     public void reset(){
         charArray.clear();
